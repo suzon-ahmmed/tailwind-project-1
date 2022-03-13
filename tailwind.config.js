81d916a7d97bd7d8521bb44003ff37e610ca3b69
suzon-ahmmed <info.suzonahamed@gmail.com>
2022-03-13 10:59:28 +0600

    update tailwind verson

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    emmetCompletions: true,
+
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
